88d00f100b5dfbdfa90a5ee6c46739a3bf9afd23
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2019-04-29 03:23:40 +0300

    Adds function checkTriangle
    -Adds main function checkTriangle.
    -Links to html 'checkresults' id.

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,13 @@ var sideA; //side A as a string
 var sideB; //side B as a string
 
 var sideC; //side C as a string
+
+
+function checkTriangle(){
+
+
+
+    document.getElementById('checkresults').innerHTML  = result;
+    
+    
+}
